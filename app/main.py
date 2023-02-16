f9410656a926bddeb9a67b66d0031f0a02bc42bc
Andre Mello <mellowcoding@gmail.com>
2023-02-16 16:38:57 -0500

    removed sqlalchemy table creation

--- app/main.py
+++ app/main.py
@@ -3,7 +3,6 @@ from .database import engine
 from fastapi import FastAPI
 from .routers import users, posts, authentication, votes
 
-models.Base.metadata.create_all(bind=engine)
 app = FastAPI()
 
 app.include_router(users.router)
