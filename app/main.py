1c6b8c989fcc762f250473b56b0346fbc2317805
Andre Mello <mellowcoding@gmail.com>
2023-02-22 14:58:06 -0500

    set up root redirect

--- app/main.py
+++ app/main.py
@@ -3,6 +3,7 @@ from .database import engine
 from fastapi import FastAPI
 from .routers import users, posts, authentication, votes
 from fastapi.middleware.cors import CORSMiddleware
+from starlette.responses import RedirectResponse
 
 app = FastAPI()
 
@@ -20,3 +21,8 @@ app.include_router(users.router)
 app.include_router(posts.router)
 app.include_router(authentication.router)
 app.include_router(votes.router)
+
+
+@app.get("/")
+def root():
+    return RedirectResponse(url='/docs')
